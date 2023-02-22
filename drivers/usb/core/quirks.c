932d5fd1db6f0a7ac71fd7775d52e4cd2e65eaaa
NotZeetaa <rodrigo2005contente@gmail.com>
2023-02-22 13:14:31 +0000

    Merge tag `v4.19.273` of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into staging
    This is the 4.19.273 stable release

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -527,6 +527,9 @@ static const struct usb_device_id usb_quirk_list[] = {
 	/* DJI CineSSD */
 	{ USB_DEVICE(0x2ca3, 0x0031), .driver_info = USB_QUIRK_NO_LPM },
 
+	/* Alcor Link AK9563 SC Reader used in 2022 Lenovo ThinkPads */
+	{ USB_DEVICE(0x2ce3, 0x9563), .driver_info = USB_QUIRK_NO_LPM },
+
 	/* VCOM device */
 	{ USB_DEVICE(0x4296, 0x7570), .driver_info = USB_QUIRK_CONFIG_INTF_STRINGS },
 
